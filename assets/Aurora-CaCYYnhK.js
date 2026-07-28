77d30075d67540d3f4bba201aebe652d1533fa76
Ryan Ghosh <111358025+HazarD000123@users.noreply.github.com>
2026-07-28 12:51:06 +0530

    Update Cultural Registration Form link across 3D overlays and region data

--- assets/Aurora-CaCYYnhK.js
+++ assets/Aurora-CaCYYnhK.js
@@ -1,4 +1,4 @@
-import{r as Pe,a as Ox,j as yt}from"./index-DIsp2zbC.js";function Bx(r){return r&&r.__esModule&&Object.prototype.hasOwnProperty.call(r,"default")?r.default:r}/**
+import{r as Pe,a as Ox,j as yt}from"./index-Brnc5Y3O.js";function Bx(r){return r&&r.__esModule&&Object.prototype.hasOwnProperty.call(r,"default")?r.default:r}/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
